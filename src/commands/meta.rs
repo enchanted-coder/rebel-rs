36ef232a4032d0b9dfc5c8e00747546a8710dfaf
daniel togey <danieltogey@pm.me>
2023-06-08 16:38:39 +0300

    added command line multiple prefixes

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,7 +1,29 @@
 use serenity::framework::standard::macros::command;
-use serenity::framework::standard::CommandResult;
+use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use serenity::utils::{content_safe, ContentSafeOptions};
+use std::fmt::Write;
+
+use crate::CommandCounter;
+
+#[command]
+#[bucket = "complicated"]
+async fn commands(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut contents = "Commands used:\n".to_string();
+
+    let data = ctx.data.read().await;
+    let counter = data.get::<CommandCounter>().expect("Expected CommandCounter in TypeMap.");
+
+    for (k, v) in counter {
+        writeln!(contents, "- {name}: {amount}", name = k, amount = v)?;
+    }
+
+    msg.channel_id.say(&ctx.http, &contents).await?;
+
+    Ok(())
+}
+
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
@@ -9,3 +31,4 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 
     Ok(())
 }
+
