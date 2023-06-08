119db1d6bd8ff7071aa42f9ae4bc01b67200f282
daniel togey <danieltogey@pm.me>
2023-06-08 14:11:46 +0300

    added command math

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
 
+mod commands;
 
 use anyhow::anyhow;
 use serenity::{
     async_trait,
-    client::{Client, Context, EventHandler},
-    framework::StandardFramework,
-    model::gateway::Ready,
-    prelude::GatewayIntents,
-    
+    client::{Client, Context, EventHandler},    
 };
 
-use tracing::info;
-
 use shuttle_secrets::SecretStore;
-
-
 use songbird::SerenityInit;
+
+use core::panic;
+use std::collections::HashSet;
+use std::env;
+
+use std::sync::Arc;
+
+use serenity::client::bridge::gateway::ShardManager;
+
+use serenity::framework::standard::macros::group;
+use serenity::framework::standard::StandardFramework;
+use serenity::http::Http;
+use serenity::model::event::ResumedEvent;
+
+use serenity::model::gateway::Ready;
+
+
+use serenity::prelude::*;
+use tracing::{error, info};
+
+use crate::commands::math::*;
+use crate::commands::meta::*;
+use crate::commands;:owner::*;
+
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
 
 
 struct Handler;
@@ -25,9 +48,15 @@ impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
     }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
 }
 
-
+#[group]
+#[commands(multipley, ping, quit)]
+struct General;
 
 #[shuttle_runtime::main]
 async fn serenity(
@@ -40,23 +69,57 @@ async fn serenity(
         return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
     };
 
+    let http = Http::new(&token);
+
+    let(owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(inf.owner.id);
+
+            (owners, info.id)
+        
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
     let framework = StandardFramework::new()
         .configure(|c| c
-                    .prefix("~"));
+                    .prefix("~"))
+                    .group(&GENRAL_GROUP);
         
 
     // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
 
-    let client = Client::builder(&token, intents)
+    let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .framework(framework)
         .register_songbird()
         .await
         .expect("Err creating client");
 
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signam::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+
     
 
-    Ok(client.into())
+    
 }
+
+
 
